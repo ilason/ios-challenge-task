e2ab137c19ee4b64a06f14706aa024bd3db1bc72
Aaron Eliason <aaroneliason@hotmail.com>
2016-04-15 13:29:58 -0600

    Initial Commit

---
+++ ios-challenge-task1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-challenge-task1
+//
+//  Created by Aaron Eliason on 4/15/16.
+//  Copyright © 2016 IlasonApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
